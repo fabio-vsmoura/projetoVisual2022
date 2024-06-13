68d774b7747bbac13d4d5d7909bd5e1ca33f5b82
fb.vsmoura@gmail.com <design.criation@gmail.com>
2024-06-13 11:13:53 -0300

    mais uma mensagem do colaborador

--- projetogitvs2022/projetogitvs2022/Form1.cs
+++ projetogitvs2022/projetogitvs2022/Form1.cs
@@ -13,7 +13,7 @@ namespace projetogitvs2022
             MessageBox.Show("Vamos continuar fazendo o nosso trabalho!");
             MessageBox.Show("trabalhar em grupo fica um pouco mais fácil assim!");
             MessageBox.Show("Postagem do colaborador de teste");
-            MessageBox.Show("Postagem do mais uma vez");
+            MessageBox.Show("Postagem do mais uma vez, bora!!");
 
 
         }
